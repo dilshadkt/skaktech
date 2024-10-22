7e14227553089131149a32b47c618f162d76df24
SanuRaj-K <sanusrk29@gmail.com>
2024-10-22 18:14:29 +0530

    feat:updating slider

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -25,14 +25,14 @@ const HomePage = () => {
     <div className=" w-full overflow-hidden overflow-x-hidden">
       {/* Banner section */}
       <section className=" w-full">
-        <div className=" relative     overflow-hidden min-h-screen bg-home bg-cover object-cover   w-full">
+        <div className=" relative      overflow-hidden min-h-screen bg-home bg-cover object-cover   w-full">
           <div className="  flex flex-col-reverse lg:flex-row lg:justify-between ">
             <motion.div
               initial={{ opacity: 0, x: -100 }}
               whileInView={{ opacity: 1, x: 0 }}
               transition={{ duration: 1 }}
               viewport={{ once: false }}
-              className="  absolute   bottom-0 z-30 flex flex-col md:text-center  lg:text-start md:items-center md:justify-center px-5 lg:px-0  w-full lg:w-1/2 mt-20 lg:ml-10  py-16"
+              className="  absolute     bottom-0 z-30 flex flex-col md:text-center mt-40  lg:text-start  px-5 lg:px-0  w-full lg:w-1/2 sm:mt-20  lg:mt-0 lg:ml-10  py-16"
             >
               <div className=" z-40 flex flex-col  md:items-center lg:items-start">
                 <span className=" lg:text-[28px] xl:text-[34px] md:max-w-[450px] xl:max-w-[600px] font-semibold text-white">
@@ -71,7 +71,9 @@ const HomePage = () => {
             </motion.div>
             {/* banner right side mobile sectio  */}
             <div
-              className={` flex flex-col items-center content-center    absolute   top-[200px] lg:top-auto   md:bottom-0 right-0 w-full lg:w-1/2`}
+              className={`  ${
+                sliderIndex !== 2 ? " top-[40px]" : " top-[100px]"
+              } flex flex-col items-center content-center      absolute    sm:top-[200px] lg:top-auto   md:bottom-0 right-0 w-full lg:w-1/2`}
             >
               <motion.div
                 initial={{ opacity: 0, x: 100 }}
