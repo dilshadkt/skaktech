501794305faea106dc45234c70bb6f61572f1d94
Shinas Muhammed <shinasmuhd43@gmail.com>
2025-01-04 14:01:07 +0530

    padding issue solved

--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -23,7 +23,7 @@ const LandingPage = () => {
           initial={{ scale: 0 }} 
           animate={{ scale: 1 }}  
           transition={{ duration: 0.6 }}
-          className=" py-10 lg:py-0 w-full  px-5 lg:px-0  flex flex-col gap-5 justify-end items-center content-center h-full  "
+          className="  lg:py-0 w-full  px-5 lg:px-0  flex flex-col  justify-end items-center content-center h-full  "
         >
           <h1 className=" text-[24px] sm:text-[30px] lg:text-[64px] max-w-[500px] lg:max-w-[1080px] text-center font-bold text-[#404040] ">
             Products You Can Rely On, Service that Makes
