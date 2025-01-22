96b7bb95af6d9e17dc2fdc47254a822418b17d49
Shinas Muhammed <shinasmuhd43@gmail.com>
2025-01-22 14:45:59 +0530

    alignment problems solved

--- src/components/productList/index.jsx
+++ src/components/productList/index.jsx
@@ -9,7 +9,7 @@ const ProductList = () => {
     <div className='py-5'>
         {allProductsData.map((item, index) => (<div key={index}>
                 <div className="grid  md:grid-cols-2 px-5 md:px-10 lg:px-20 py-8 md:py-20 gap-10 items-stretch ">
-                  <div className={`h-[40vh]  ${index % 2 === 0 ? 'xl:order-1' : 'xl:order-2'}` }>
+                  <div className={`  ${index % 2 === 0 ? 'xl:order-1' : 'xl:order-2'}` }>
                     <Image src={`/assets/images/${item.img}.png`} alt="serviceimg" height={700} width={600} quality={100} className="h-full object-cover"/>
                   </div>
                   <div
